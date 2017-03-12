0a0ebbb5e6dd1fc6c9ff9264d53df9530d62e3f0
Tahvohck <Tahvohck@gmail.com>
2017-03-12 14:42:08 -0400

    Forgot the actual like, important file.

---
+++ Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Planetbase;
+
+namespace CommunityUtils
+{
+    public class CUtils
+    {
+		/// <summary>
+		/// Register a new component into the <see cref="Planetbase.ComponentTypeList"/> so that it can be added to modules later.
+		/// </summary>
+		/// <returns><see cref="System.Boolean"/>: true if sucessful, false if not. (To be implemented)</returns>
+		public static void registerComponent(ComponentType newCompType) {
+			TypeList<ComponentType, ComponentTypeList>.getInstance().add(newCompType);
+		}
+
+		public static void addComponentToModule<CompT,ModuleT>() 
+			where CompT : ComponentType 
+			where ModuleT : ModuleType {
+			ModuleT module = TypeList<ModuleType, ModuleTypeList>.find<ModuleT>() as ModuleT;
+			List<ComponentType> components = module.mComponentTypes.ToList();
+			components.Insert(3, TypeList<ComponentType, ComponentTypeList>.find<CompT>());
+			module.mComponentTypes = components.ToArray();
+        }
+    }
+}
